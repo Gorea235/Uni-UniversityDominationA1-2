32637cc3a6488544800e26b59dd3fd76162bab86
Luke Davies <lkdvs3@gmail.com>
2017-11-13 14:21:30 +0000

    Added default scripts matching architecture

---
+++ Assets/Scripts/Map/IUnit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IUnit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
