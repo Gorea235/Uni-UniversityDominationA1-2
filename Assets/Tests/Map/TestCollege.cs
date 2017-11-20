8c1a14362a31217e00a9dceda0c965939f2298c5
Skyteens <Skyteens@users.noreply.github.com>
2017-11-20 17:56:09 +0000

    new files

---
+++ Assets/Tests/Map/TestCollege.cs
@@ -0,0 +1,7 @@
+using System;  
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+
+namespace TestCollege 
+{
+    
+}
---
+++ Assets/Tests/Map/TestMap.cs
@@ -0,0 +1,9 @@
+using System;  
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
+
+namespace TestMap
+{
+    public class maptest
+    {}
+    
+}
