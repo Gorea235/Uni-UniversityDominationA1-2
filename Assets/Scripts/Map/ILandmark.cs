284ee189bce89d8f8873e77e56ae0d7af9de8ec1
A. Simeonov <angel.st.sim@gmail.com>
2017-11-20 19:54:47 +0000

    Manual conflict resolution in ILandmark from previously auto generated conflict code

--- Assets/Scripts/Map/ILandmark.cs
+++ Assets/Scripts/Map/ILandmark.cs
@@ -10,13 +10,7 @@ namespace Map
     public interface ILandmark
     {
 
-<<<<<<< HEAD
-    void StartBonus(IPlayer player, Coord coord, Context context);
-    void TurnBonus(IPlayer player, Coord coord, Context context);
-}
-=======
         void StartBonus(IPlayer player, Coord coord, Context context);
         void TurnBonus(IPlayer player, Coord coord, Context context);
     }
 }
->>>>>>> e66dacfb7bfa2274eebfed86d7a6bdb476c598eb
