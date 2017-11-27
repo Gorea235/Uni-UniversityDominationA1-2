35ed66833a13f116c9c4aaa13bae626d8b38cda4
A. Simeonov <angel.st.sim@gmail.com>
2017-11-27 20:31:59 +0000

    Added very basic serilization for the map in SCoord and Grid

--- Assets/Scripts/Map/Grid.cs
+++ Assets/Scripts/Map/Grid.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using Map.Hex;
 using UnityEngine;
+using System.IO;
 
 namespace Map
 {
     public class Grid : MonoBehaviour
     {
         Dictionary<Coord, Sector> _gridStore = new Dictionary<Coord, Sector>();
+        string mapDataFileName; // TODO: Map .json to here
 
         public Sector this[Coord coord]
         {
@@ -21,6 +23,22 @@ namespace Map
                 _gridStore[coord] = value;
             }
         }
+
+        private void LoadMapData() {
+            string filePath = Path.Combine("somepathtomapdata", mapDataFileName);
+            if (File.Exists(filePath))
+            {
+                string mapAsJson = File.ReadAllText(filePath);
+                SCoord loadedMap = JsonUtility.FromJson<SCoord>(mapAsJson);
+            }
+            else
+            {
+                throw new System.Runtime.Serialization.SerializationException();
+            }
+         }
+        
+
+
 
 
         // Use this for initialization
