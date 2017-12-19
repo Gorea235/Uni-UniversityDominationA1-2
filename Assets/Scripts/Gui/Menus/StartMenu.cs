01851d8df060a7a080470bb8209ae66468ad603f
A. Simeonov <angel.st.sim@gmail.com>
2017-12-19 15:06:00 +0200

    Added 'StartMenu Scene' with a basic UI menu that changes to 'Main Scene'. Simple logic is added in StartMenu script

---
+++ Assets/Scripts/Gui/Menus/StartMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Gui.Menus
+{
+    public class StartMenu: MonoBehaviour
+    {
+        public void LoadGameScene() {
+            SceneManager.LoadScene("Main Scene");
+        }
+    }
+}
