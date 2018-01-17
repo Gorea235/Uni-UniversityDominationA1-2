7535f7dad031e67515893609114214133419973a
bnp502 <32845119+bnp502@users.noreply.github.com>
2018-01-17 00:41:49 +0000

    Added Minecraft font, first pass of build menu with open/close buttons

---
+++ Assets/Scripts/Gui/Menus/BuildMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BuildMenu : MonoBehaviour {
+
+	[SerializeField] private GameObject _panel;
+	[SerializeField] private GameObject _buttonOpen;
+	[SerializeField] private GameObject _buttonClose;
+
+	void Start()
+	{
+		Button open = _buttonOpen.GetComponent<Button> ();
+		Button close = _buttonClose.GetComponent<Button> ();
+		open.onClick.AddListener (OpenClick);
+		close.onClick.AddListener (CloseClick);
+	}
+
+	void OpenClick()
+	{
+		_panel.SetActive (true);
+		Debug.Log ("Open button clicked");
+	}
+
+	void CloseClick()
+	{
+		_panel.SetActive (false);
+		Debug.Log ("Close button clicked");
+	}
+}
